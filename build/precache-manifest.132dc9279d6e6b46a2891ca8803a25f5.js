bc45b6193d0d4ac8a82b2f1eef2ac947ed9b27a2
minheekangg <minheekangg@gmail.com>
2019-02-22 18:39:47 -0500

    deploy

---
+++ build/precache-manifest.132dc9279d6e6b46a2891ca8803a25f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24e44fcfe7723451ed48",
+    "url": "/static/css/main.51a1a0ab.chunk.css"
+  },
+  {
+    "revision": "24e44fcfe7723451ed48",
+    "url": "/static/js/main.24e44fcf.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "aed77d6f8944f8436cb7",
+    "url": "/static/js/2.aed77d6f.chunk.js"
+  },
+  {
+    "revision": "373d535db4f5cdd15373f6a4e6c5a018",
+    "url": "/index.html"
+  }
+];
