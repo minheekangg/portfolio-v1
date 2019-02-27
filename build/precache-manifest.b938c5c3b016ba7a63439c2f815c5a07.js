4bfe9d91cc27995ed5cc60edde2e5164944bcff6
minheekangg <minheekangg@gmail.com>
2019-02-27 16:58:16 -0500

    deploy1

---
+++ build/precache-manifest.b938c5c3b016ba7a63439c2f815c5a07.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "48e6c8a43319798d26d6",
+    "url": "/static/css/main.21fca331.chunk.css"
+  },
+  {
+    "revision": "48e6c8a43319798d26d6",
+    "url": "/static/js/main.48e6c8a4.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d63409321140b1f957b0",
+    "url": "/static/js/2.d6340932.chunk.js"
+  },
+  {
+    "revision": "8a9b18a30ddc4706749ca1be2fb3cb13",
+    "url": "/static/media/capsule2.8a9b18a3.png"
+  },
+  {
+    "revision": "1d3e63f332be67142c191627765919c3",
+    "url": "/static/media/capsule1.1d3e63f3.png"
+  },
+  {
+    "revision": "67011a611bb93b6f6d0f09b67c90b68b",
+    "url": "/static/media/korean101.67011a61.png"
+  },
+  {
+    "revision": "8a3e2054ce2b84fbca1ebf3dc6ef8c73",
+    "url": "/static/media/paris.8a3e2054.jpg"
+  },
+  {
+    "revision": "ecac7f4a4230eca528a210f5858af368",
+    "url": "/index.html"
+  }
+];
